1b1ef74de7c46d668dced4be1fed362aa2776600
Gean da Silva Santos <gss@ic.ufal.br>
2015-08-12 17:31:17 -0300

    Revised test file
    Defined two main functions:
      * to DBHandle class
      * to ProductInfo class

--- application/src/test.py
+++ application/src/test.py
@@ -5,34 +5,31 @@ from product_info import ProductInfo
 USER='guest'
 PASS='123'
 HOST='localhost'
-config=(USER, PASS, HOST)
-
-#class TestDBHandle(unittest.TestCase):
-#def __init__ (self):
 dbh = DBHandle(USER, PASS, HOST)
 datalog = ('leite pó', 3.0, 10, (8,8,2015), 'Atacadão', 'yes')
 datalog2 = (('leite pó', 3.0, 10, (8,8,2015), 'Atacadão', 'yes'),
             ('macarrão', 1.5, 8, (8,8,2015), 'Atacadão', 'yes'))
 
-
-def test_insert ():
+def test_DBHandle_insert_query ():
     assertEqual(dbh.bulk_insert(*datalog), 0)
     assertEqual(dbh.bulk_insert(*datalog2), 0)
 
-    
-def test_query ():
-    dbh.bulk_insert(*datalog)
-    dbh.bulk_insert(*datalog2)
 
-
-
-#class TestProductInfo(unittest.TestCase):
-def test_add_pop():
+def test_ProductInfo_add_pop_size():
     x = ProductInfo()
-    data = ('feijão', 3.5, 3, (2015,8,8), 'Atacadão', 'yes')
-    x.add(*data)
-    assertEqual(data, x.pop())
-
+    x.add(*datalog)
+    assertEqual(datalog, x.pop())
+    assertEqual(x.size(), 0)
+    x.add(*datalog2[0])
+    assertEqual(x.size(), 1)
+    x.add(*datalog2[1])
+    assertEqual(x.size(), 2)
+    assertEqual(datalog2[0], x.pop())
+    assertEqual(x.size(), 1)
+    assertEqual(datalog2[1], x.pop())
+    assertEqual(x.size(), 0)
+    assertEqual(None, x.pop())
+    assertEqual(x.size(), 0)
 
 
 if __name__ == '__main__':
