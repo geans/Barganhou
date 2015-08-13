3487bc54ff4422708fe9c47305a0aeb92299baf9
geans <geans.santos@gmail.com>
2015-08-12 23:56:18 -0300

    Add python cache to gitignore Changes variable name in test class Add git-pip.py file

--- application/src/test.py
+++ application/src/test.py
@@ -6,30 +6,40 @@ USER='guest'
 PASS='123'
 HOST='localhost'
 dbh = DBHandle(USER, PASS, HOST)
-datalog = ('leite pó', 3.0, 10, (8,8,2015), 'Atacadão', 'yes')
+datalog1 = ('leite pó', 3.0, 10, (8,8,2015), 'Atacadão', 'yes')
 datalog2 = (('leite pó', 3.0, 10, (8,8,2015), 'Atacadão', 'yes'),
             ('macarrão', 1.5, 8, (8,8,2015), 'Atacadão', 'yes'))
 
-def test_DBHandle ():
-    assertEqual(dbh.bulk_insert(*datalog), 0)
-    assertEqual(dbh.bulk_insert(*datalog2), 0)
+class TestCases (unittest.TestCase):
 
+    def test_DBHandle (self):
+        pi1 = ProductInfo()
+        pi2 = ProductInfo()
+        pi1.add(*datalog1)
+        pi2.add(*datalog2[0])
+        pi2.add(*datalog2[1])
+        self.assertEqual(dbh.bulk_insert(pi1), 0)
+        self.assertEqual(dbh.bulk_insert(pi2), 0)
 
-def test_ProductInfo ():
-    x = ProductInfo()
-    x.add(*datalog)
-    assertEqual(datalog, x.pop())
-    assertEqual(x.size(), 0)
-    x.add(*datalog2[0])
-    assertEqual(x.size(), 1)
-    x.add(*datalog2[1])
-    assertEqual(x.size(), 2)
-    assertEqual(datalog2[0], x.pop())
-    assertEqual(x.size(), 1)
-    assertEqual(datalog2[1], x.pop())
-    assertEqual(x.size(), 0)
-    assertEqual(None, x.pop())
-    assertEqual(x.size(), 0)
+    def test_ProductInfo (self):
+        pi1 = ProductInfo()
+        pi2 = ProductInfo()
+        self.assertEqual(pi1.size(), 0)
+        self.assertEqual(pi2.size(), 0)
+        pi1.add(*datalog1)
+        pi2.add(*datalog2[0])
+        pi2.add(*datalog2[1])
+        self.assertEqual(pi1.size(), 1)
+        self.assertEqual(pi2.size(), 2)
+        self.assertEqual(pi1.pop(), datalog1)
+        self.assertEqual(pi1.size(), 0)
+        self.assertEqual(pi2.pop(), datalog2[0])
+        self.assertEqual(pi2.size(), 1)
+        self.assertEqual(pi2.pop(), datalog2[1])
+        self.assertEqual(pi2.size(), 0)
+        self.assertEqual(pi2.pop(), None)
+        self.assertEqual(pi2.size(), 0)
+
 
 
 if __name__ == '__main__':
