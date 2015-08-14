b6244457c5d88b1a61dd9a270faa7b00d662bdd2
geans <geans.santos@gmail.com>
2015-08-14 11:42:44 -0300

    criado view nova usando template

--- application/barganhou/register/urls.py
+++ application/barganhou/register/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<product_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<product_id>[0-9]+)/price/$', views.detail, name='price'),
+    url(r'^(?P<product_id>[0-9]+)/local/$', views.detail, name='local'),
 ]
--- application/barganhou/register/views.py
+++ application/barganhou/register/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+from .models import ProductInfo
 
-# Create your views here.
 def index (request):
-    return HttpResponse("Ôpa! Estás na primeira página do Registro de Informações de Produtos")
+    latest_product_list = ProductInfo.objects.order_by('-date_log')[:5]
+    template = loader.get_template('register/index.html')
+    context = RequestContext(request, {
+        'latest_product_list': latest_product_list,
+    })
+    return render(request, 'polls/index.html', context)
 
 def detail (request, product_id):
     return HttpResponse("Você está vendo detalhes do produto %s." % product_id)
+
+def price (request, product_id):
+    return HttpResponse("Você está vendo o preço do produto %s." % product_id)
+    
+def local (request, product_id):
+    return HttpResponse("Você está vendo o local de registro do produto %s." % product_id)
