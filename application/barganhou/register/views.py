884ed4be7dcf0f3334f03f829257f1af26831f83
geans <geans.santos@gmail.com>
2015-08-15 10:28:16 -0300

    Adicionado formato de moeda ao view e detail Adicionado pasta django ao gitignore

--- application/barganhou/register/views.py
+++ application/barganhou/register/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, Http404
 from django.shortcuts import get_object_or_404, render
 from django.template import RequestContext, loader
 from .models import ProductInfo
+import locale
 
 def index (request):
     latest_product_list = ProductInfo.objects.order_by('-date_log')[:5]
@@ -13,7 +14,10 @@ def index (request):
 
 def detail (request, product_id):
     product = get_object_or_404(ProductInfo, pk=product_id)
-    return render(request, 'register/detail.html', {'product': product})
+    price = locale.format("%1.2f", product.price, 1)
+    locale.setlocale(locale.LC_ALL, "")
+    return render(request, 'register/detail.html',
+        {'product': product, 'price': price})
 
 def price (request, product_id):
     return HttpResponse("Você está vendo o preço do produto %s." % product_id)
